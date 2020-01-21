52d8fcdbb2a230a8055ca7253ea81fd3934e67a2
Samsruti Dash <sam.sipun@gmail.com>
2020-01-22 03:09:44 +0530

    sample screen2 added

--- app/src/main/java/com/byjus/headlines/assignment/samsruti/ui/headlines/HeadlinesFragment.kt
+++ app/src/main/java/com/byjus/headlines/assignment/samsruti/ui/headlines/HeadlinesFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.navigation.fragment.findNavController
 
 import com.byjus.headlines.assignment.samsruti.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HeadlinesFragment : Fragment() {
 
@@ -21,7 +24,11 @@ class HeadlinesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.headlines_fragment, container, false)
+        val root = inflater.inflate(R.layout.headlines_fragment, container, false)
+        root.findViewById<LinearLayout>(R.id.baseitem).setOnClickListener {
+            this.findNavController().navigate(HeadlinesFragmentDirections.actionHeadlinesFragmentToDetailsFragment())
+        }
+        return root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
